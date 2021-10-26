8e915b505899be62be94e0aaae8ec81b5029b335
Mussa Charles <mussacharles60@gmail.com>
2021-10-26 19:29:04 +0300

    update user interface

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -17,14 +17,20 @@ export default class Main extends React.Component<
     is_initializing: boolean;
     is_running_process: boolean;
     has_media_stream: boolean;
-    videoWidth: number;
-    videoHeight: number;
+    show_canvas: boolean;
+    // videoWidth: number;
+    // videoHeight: number;
   }
 > {
   private didMount: boolean = false;
   private webcam: React.RefObject<Webcam> = React.createRef<Webcam>();
   private mainView: React.RefObject<HTMLDivElement> =
     React.createRef<HTMLDivElement>();
+
+  private canvas1: React.RefObject<HTMLCanvasElement> =
+    React.createRef<HTMLCanvasElement>();
+  private canvas2: React.RefObject<HTMLCanvasElement> =
+    React.createRef<HTMLCanvasElement>();
 
   constructor(props: any) {
     super(props);
@@ -36,11 +42,12 @@ export default class Main extends React.Component<
       is_initializing: true,
       is_running_process: false,
       has_media_stream: false,
-      videoWidth: 200,
-      videoHeight: 700,
+      show_canvas: false,
+      // videoWidth: 200,
+      // videoHeight: 700,
     };
 
-    // this.onSessionIdChanged = this.onSessionIdChanged.bind(this);
+    this.capture = this.capture.bind(this);
     // this.onStartSessionBtnClick = this.onStartSessionBtnClick.bind(this);
     // this.onStopSessionBtnClick = this.onStopSessionBtnClick.bind(this);
   }
@@ -49,10 +56,10 @@ export default class Main extends React.Component<
     this.didMount = true;
     if (this.mainView && this.mainView.current) {
       // const imageSrc = this.webcam.current.getScreenshot();
-      this.setState({
-        videoWidth: this.mainView.current.getBoundingClientRect().width,
-        videoHeight: this.mainView.current.getBoundingClientRect().height,
-      });
+      // this.setState({
+      //   videoWidth: this.mainView.current.getBoundingClientRect().width,
+      //   videoHeight: this.mainView.current.getBoundingClientRect().height,
+      // });
     }
     try {
       (window as any).MdmAndroid.onInitializationBegin();
@@ -139,49 +146,98 @@ export default class Main extends React.Component<
     ]);
   }
 
-  private capture(): void {
-    if (this.webcam && this.webcam.current) {
+  private async capture() {
+    const mtcnnParams: any = {
+      // number of scaled versions of the input image passed through the CNN
+      // of the first stage, lower numbers will result in lower inference time,
+      // but will also be less accurate
+      maxNumScales: 10,
+      // scale factor used to calculate the scale steps of the image
+      // pyramid used in stage 1
+      scaleFactor: 0.709,
+      // the score threshold values used to filter the bounding
+      // boxes of stage 1, 2 and 3
+      scoreThresholds: [0.6, 0.7, 0.7],
+      // mininum face size to expect, the higher the faster processing will be,
+      // but smaller faces won't be detected
+      minFaceSize: 20,
+    };
+    const videoInput = document.getElementById("video-input");
+    if (this.webcam && this.webcam.current && videoInput) {
       // const imageSrc = this.webcam.current.getScreenshot();
+
+      const mtcnnResults = await faceapi.mtcnn(
+        videoInput as any,
+        mtcnnParams
+        //    {
+        //   minFaceSize: 20,
+        //   scaleFactor: 0.709,
+        //   maxNumScales: 10,
+        //   scoreThresholds: [0.6, 0.7, 0.7],
+        // }
+      );
+
+      console.log(mtcnnResults);
+
+      // minFaceSize?: number;
+      // scaleFactor?: number;
+      // maxNumScales?: number;
+      // scoreThresholds?: number[];
+      // scaleSteps?: number[];
     }
   }
 
   render() {
     return (
       <div className="mdm-main" ref={this.mainView}>
-        {/* <div>
-          <i
-            className="las la-spinner la-spin progress-icon"
-            style={{
-              display: this.state.is_initializing ? "flex" : "none",
-            }}
-          ></i>
-        </div> */}
         <div className="webcam-cont">
-          <Webcam
-            className="webcam-view"
-            audio={false}
-            mirrored={true}
-            width={"100%"}
-            height={"100%"}
-            ref={this.webcam}
-            screenshotFormat="image/jpeg"
-            videoConstraints={{
-              // width: this.state.videoWidth,
-              // height: this.state.videoHeight,
-              facingMode: "user",
-            }}
-            onUserMedia={() => {
-              this.setState({ has_media_stream: true });
-            }}
-            onUserMediaError={() => {
-              this.setState({ has_media_stream: false });
-            }}
-          />
-          {/* <button onClick={capture}>Capture photo</button> */}
-          {/* <div className="image-frame">
-            <canvas className="img-input" id="canvas" />
-            <canvas className="img-input canvas-output" id="canvas-output" />
-          </div> */}
+          {!this.state.is_initializing && (
+            <Webcam
+              className="webcam-view"
+              id={"video-input"}
+              audio={false}
+              mirrored={true}
+              width={"100%"}
+              height={"100%"}
+              ref={this.webcam}
+              screenshotFormat="image/jpeg"
+              videoConstraints={{
+                // width: this.state.videoWidth,
+                // height: this.state.videoHeight,
+                facingMode: "user",
+              }}
+              onUserMedia={() => {
+                setTimeout(() => {
+                  if (!this.didMount) return;
+                  this.setState({ has_media_stream: true });
+                  try {
+                    (window as any).MdmAndroid.onCameraReady();
+                  } catch (error) {
+                    console.log(error);
+                  }
+                }, 1500);
+                this.capture();
+              }}
+              onUserMediaError={() => {
+                setTimeout(() => {
+                  if (!this.didMount) return;
+                  this.setState({ has_media_stream: false });
+                  try {
+                    (window as any).MdmAndroid.onCameraFailed();
+                  } catch (error) {
+                    console.log(error);
+                  }
+                }, 1500);
+              }}
+            />
+          )}
+          <div
+            className="image-frame"
+            style={{ display: this.state.show_canvas ? "flex" : "none" }}
+          >
+            <canvas className="canvas" ref={this.canvas1} />
+            <canvas className="canvas canvas-output" ref={this.canvas2} />
+          </div>
           {this.state.is_initializing && !this.state.has_media_stream && (
             <div className="lazy-load-view">
               <i
